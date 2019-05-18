e8d9fe6b3b62be18ef1e2968f9d9c1a54471a2d6
Raikume <nakurakanra@hotmail.com>
2019-05-18 23:43:22 +0100

    Created ANN and forward propagation

---
+++ main.py
@@ -0,0 +1,87 @@
+from random import random
+import math
+
+#  takes an array which specify the number of nodes for each layer and creates an Artificial Neural Network
+#  starting weights are random
+#  model[layers][nodes]
+#  model[layers][nodes]['a'] activation
+# model[layers][nodes]['w'][1,2,3] weights
+def rmatdot(r,c):
+    m1 = r
+    m2 = []
+    p = []
+    ##  make a new array from c for m2 to for loop the maxtrix multiplication
+    for x in range(0,len(c[0])):
+        m2.append([])
+        for y in range(0,len(c)):
+            m2[x].append(c[y][x])
+##    print(m2)
+    for x in range(0,len(m2)):
+        t = 0
+        for y in range(0,len(m2[x])):
+            t += m1[y]*m2[x][y]
+        p.append(t)
+    return p
+
+def matdot(m1,m2):
+    if len(m1[0]) != len(m2):
+        return False
+    p = []
+    for x in range(0,len(m1)):
+        p.append(rmatdot(m1[x],m2))
+    return p
+
+def sigmoid(x):
+    return 1/(1+math.exp(-x))
+
+def buildANN(nodes = []):
+    model = []
+    layers = len(nodes)
+    weights = layers-1
+
+    for x in range(0,layers):
+        model.append([])
+        for y in range(0,nodes[x]):
+            model[x].append([])
+            model[x][y] = {}
+            model[x][y]['a'] = 0
+            if x < weights:
+                model[x][y]['w'] = []
+                for z in range(0,nodes[x+1]):
+                    model[x][y]['w'].append(int(random()*100)/100)
+    return model
+
+def setInputs(model,inputs = []):
+    if len(model[0]) != len(inputs):
+        return False
+    for x in range(0,len(inputs)):
+        model[0][x]['a'] = inputs[x]
+    return model
+
+def getOutputs(model):
+    return model[len(model)-1]
+#  calculate the next layer's activation function based on the previous layers, and should return an array for l2 next activation function
+#  build a row vector, then column vectors for the weights and then perform matrix multiplication
+def forward(model):
+    layers = len(model)
+    for x in range(0,layers-1):
+        a1 = []
+        w = []
+        for y in range(0,len(model[x])):
+            a1.append(model[x][y]['a'])
+            w.append(model[x][y]['w'])
+            a2 = rmatdot(a1,w)
+            for z in range(0,len(a2)):
+                model[x+1][z]['a'] = sigmoid(a2[z])
+    return model
+
+
+
+
+
+print("Running ANN")
+ann = buildANN([2,2])
+
+ann = setInputs(ann,[2,3])
+ann = forward(ann)
+print(*getOutputs(ann))
